9de60b8de7a16160bdf24e26dcb60ba47d432752
IkuoShige <s21c1060dq@s.chibakoudai.jp>
2023-08-10 21:51:11 +0900

    Add function that convert mp3 to m4a, m4a to mp3

--- convert.py
+++ convert.py
@@ -1,10 +1,26 @@
 import glob, os
 
-# .m4a→.mp3 m4a_dataにある.m4aの音声データをmp3_dataへmp3として保存する
-for filename in glob.glob( 'm4a_data/*.m4a' ):
-    a = filename[len("m4a_data")+1:][:-4] # ファイル名だけ取り出す ex) m4a_data/hoge.m4a → hoge
-    os.system("ffmpeg -i m4a_data/{}.m4a -ab 256k mp3_data/{}.mp3".format(a,a))
+def convert_m4a_to_mp3():
+    # .m4a→.mp3 m4a_dataにある.m4aの音声データをmp3_dataへmp3として保存する
+    for filename in glob.glob( 'm4a_data/*.m4a' ):
+        a = filename[len("m4a_data")+1:][:-4] # ファイル名だけ取り出す ex) m4a_data/hoge.m4a → hoge
+        os.system("ffmpeg -i m4a_data/{}.m4a -ab 256k mp3_data/{}.mp3".format(a,a))
 
-#for filename in glob.glob( 'mp3_data/*.mp3' ):
-#    a = filename[len("mp3_data")+1:][:-4] # ファイル名だけ取り出す ex) mp3_data/hoge.mp3 → hoge
-#    os.system("sox mp3_data/{}.mp3 data_wav/{}.wav channels 1 rate 16k".format(a,a))
+import subprocess
+
+def convert_mp3_to_m4a(input_file, output_file):
+    command = [
+        'ffmpeg',
+        '-i', input_file,
+        '-c:a', 'aac',
+        '-strict', 'experimental',
+        output_file
+    ]
+
+    subprocess.run(command)
+
+if __name__ == "__main__":
+    input_mp3_file = 'sample.mp3'  # 変換したいMP3ファイルのパス
+    output_m4a_file = 'sample.m4a'  # 変換後のM4Aファイルのパス
+    convert_mp3_to_m4a(input_mp3_file, output_m4a_file)
+
